59b409dbb50b9067955af32a50a1511ebabe44e0
Roman Medvedev <roman.medvedev77@gmail.com>
2022-07-18 15:08:57 -0400

    add invertTree

--- leetcode/e-invertTree.js
+++ leetcode/e-invertTree.js
@@ -4,32 +4,6 @@ function TreeNode(val, left, right) {
   this.right = right === undefined ? null : right;
 }
 
-class Tree {}
-
-// node class
-class Node extends Tree {
-  constructor(left, v, right) {
-    super();
-    this.v = v;
-    this.left = left;
-    this.right = right;
-  }
-}
-
-// leaf class
-
-class Leaf extends Tree {
-  constructor(v) {
-    super();
-    this.v = v;
-  }
-}
-new Node(
-  new Node(new Leaf(7), 3, new Leaf(3)),
-  6,
-  new Node(new Leaf(8), 4, new Leaf(1))
-);
-
 const invertTree = (root) => {
   if (root !== null) {
     let temp = root.left;
