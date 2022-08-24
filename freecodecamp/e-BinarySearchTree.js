aafc71ded1f1c6b8667bb48b2b8119726130a570
Roman Medvedev <roman.medvedev77@gmail.com>
2022-08-24 15:43:18 -0400

    add traverse through tree inorder, preorder, postorder

--- freecodecamp/e-BinarySearchTree.js
+++ freecodecamp/e-BinarySearchTree.js
@@ -97,5 +97,50 @@ function BinarySearchTree() {
   this.isBalanced = function () {
     return this.findMinHeight() == this.findMaxHeight();
   };
+
+  this.inorder = function () {
+    if (!this.root) return null;
+
+    function traverse(node) {
+      if (!node) return [];
+      const nodes = traverse(node.left); // Left
+      nodes.push(node.value); // Root (in-order)
+      nodes.push(...traverse(node.right)); // Right
+      return nodes;
+    }
+
+    return traverse(this.root);
+  };
+
+  // Pre-order traversal
+  this.preorder = function () {
+    if (!this.root) return null;
+
+    function traverse(node) {
+      if (!node) return [];
+      const nodes = [node.value]; // Root (pre-order)
+      nodes.push(...traverse(node.left)); // Left
+      nodes.push(...traverse(node.right)); // Right
+      return nodes;
+    }
+
+    return traverse(this.root);
+  };
+
+  // Post-order traversal
+  this.postorder = function () {
+    if (!this.root) return null;
+
+    function traverse(node) {
+      if (!node) return [];
+      const nodes = traverse(node.left); // Left
+      nodes.push(...traverse(node.right)); // Right
+      nodes.push(node.value); // Root (post-order)
+      return nodes;
+    }
+
+    return traverse(this.root);
+  };
+
   // Only change code above this line
 }
