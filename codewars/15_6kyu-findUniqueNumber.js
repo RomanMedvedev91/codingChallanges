66eb56f19480fc944c8990c532fe6b119ffe5ddb
Roman Medvedev <roman.medvedev77@gmail.com>
2023-01-25 22:37:02 -0500

    add find unique number 2nd way

--- codewars/15_6kyu-findUniqueNumber.js
+++ codewars/15_6kyu-findUniqueNumber.js
@@ -1,5 +1,9 @@
 function findUniq(arr) {
-  return arr.find((item) => arr.indexOf(item) === arr.lastIndexOf(item))
+  return arr.find((item) => arr.indexOf(item) === arr.lastIndexOf(item));
+
+  // 2nd way
+  // arr.sort((a,b)=>a-b);
+  // return arr[0]==arr[1]?arr.pop():arr[0];
 }
 
 module.exports = findUniq;
